76b5755f1969c52db0417062400c2a509ae5995b
007khun159 <008khun159@gmail.com>
2022-09-29 21:32:25 +0700

    fix add host debug and port

--- Guest_book7.py
+++ Guest_book7.py
@@ -32,5 +32,6 @@ def process():
     db.session.commit()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    
+    app.run(host = '0.0.0.0',port = 80,debug = True)
