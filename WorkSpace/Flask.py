4a4890ec923a735065a6341ab6b10bba78406235
007khun159 <008khun159@gmail.com>
2022-10-15 14:10:59 +0700

    update

--- WorkSpace/Flask.py
+++ WorkSpace/Flask.py
@@ -8,4 +8,4 @@ def index():
     return render_template('page.html')
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0',port = 500211,debug = True)
+    app.run(host ='0.0.0.0',port = 80,debug = True)
