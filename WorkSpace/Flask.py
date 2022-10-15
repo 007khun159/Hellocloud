61f456ada623bdd7cb1f7d886c4ceccfdd394ebb
007khun159 <008khun159@gmail.com>
2022-10-15 13:24:48 +0700

    upadate

--- WorkSpace/Flask.py
+++ WorkSpace/Flask.py
@@ -2,9 +2,11 @@ from flask import Flask,render_template
 
 app = Flask (__name__)
 
+
 @app.route('/')
 def index():
     return render_template('page.html')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port = 80,debug = True)
+    app.run(debug = True)
+    app.run(host ='0.0.0.0',port = 80)
