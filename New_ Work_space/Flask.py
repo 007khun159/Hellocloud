f9243ef579d9afa665ed69c3d07df4ab7eb94b7b
007khun159 <008khun159@gmail.com>
2022-10-23 11:48:10 +0700

    update new

--- New_ Work_space/Flask.py
+++ New_ Work_space/Flask.py
@@ -27,11 +27,11 @@ def index():
 
 @app.route('/process',methods =['Post'])
 def process():
-    id = request.form['student_id']
+    student_id= request.form['student_id']
     f_name = request.form['f_name']
     l_name = request.form['l_name']
     e_mail = request.form['e_mail']
-    signature = Students(id=id , f_name = f_name , l_name = l_name , e_mail = e_mail)
+    signature = Students(student_id=student_id , f_name = f_name , l_name = l_name , e_mail = e_mail)
     db.session.add(signature)
     db.session.commit()
     return redirect(url_for('index'))
