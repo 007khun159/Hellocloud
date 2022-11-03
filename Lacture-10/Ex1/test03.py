68d3b6c498506880d67a9427d443f6218ca7ac15
007khun159 <008khun159@gmail.com>
2022-11-03 14:16:20 +0700

    up

--- Lacture-10/Ex1/test03.py
+++ Lacture-10/Ex1/test03.py
@@ -1,3 +1,5 @@
+
+from pydoc import describe
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -9,6 +11,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
 #Init db
 db = SQLAlchemy(app)
 #Init ma
@@ -22,7 +25,7 @@ class Product(db.Model):
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
 
-    def init(self, name, description, price, qty ):
+    def __init__(self, name, description, price, qty ):
         self.name = name
         self.description = description
         self.price = price
@@ -33,10 +36,81 @@ class ProductSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'description', 'price', 'qty')
 
+#POST product 
+@app.route('/product',methods = ['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price =  request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name,description,price,qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+
+
+db.create_all()
+
+
+
 #Init Schema
+#ใช้ในกรณีมีสินค้าหลายอัน
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 db.create_all()
+
+#Get All Product
+# POST = ใส่ข้อมูล 
+# GET  = จัดการข้อมูล
+
+@app.route('/product',methods = ['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+#GET select
+
+@app.route('/product/<id>',methods = ['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
+
+#Update a Product 
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.desciption = description
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+#Del
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsoninfy(product)
+
+
+
 #Run Server
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ Lacuture-11/E5-thredpool.py
@@ -0,0 +1,20 @@
+import logging
+import threading
+import concurrent.futures
+import time
+
+def thread_function(name):
+    logging.info('Thread %s: starting' ,name)
+    time.sleep(2)
+    logging.info("Thread %s: finsishing",name)
+
+if __name__ == '__main':
+    format ="%(asctime)s: %(message)%s"
+    logging.basicConfig(format=format , level = logging.INFO, datefmt = "%H %M %S")
+
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3)as executor:
+        executor.map(thread_function,range(3))
+
+
+        
